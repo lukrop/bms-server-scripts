4aa3b6154d5588128e9f5fa3aa538c0a514071cf
Lukas Kropatschek <lukas@kropatschek.net>
2019-03-18 11:53:10 +0100

    fix logfile path

--- prometheus/bms-fps.sh
+++ prometheus/bms-fps.sh
@@ -4,7 +4,7 @@
 # Author(s): Lukas Kropatschek <lukas@kropatschek.net>
 # License: See LICENSE
 
-BMS_LOGFILE=${BMS_LOGFILE:-"../logs/bms.log"}
+BMS_LOGFILE=${BMS_LOGFILE:-"logs/bms.log"}
 
 echo "# HELP bms_fps Falcon BMS FPS running with wine"
 echo "# TYPE bms_fps gauge"
@@ -14,7 +14,7 @@ then
 	echo "# BMS is not running"
 	echo "# bms_fps 0"
 else 
-	FPS=`cat $BMS_LOGFILE | sort -r | grep -m1 "trace:fps" | \
+	FPS=`cat $BMS_LOGFILE | sort -r | grep -m1 "trace:fps" |\
 	       	grep -o '[0-9]*\.[0-9].fps' | cut -d'.' -f1`
 	echo bms_fps $FPS
 fi
